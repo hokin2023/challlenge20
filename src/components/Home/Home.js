a0065e1be9fb99d46ac73745564ee0e1a6570008
hokin2023 <hokin990@gmail.com>
2023-07-16 20:20:46 -0700

    added aboutme component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/kinAva.jpg";
+import SocialMedia from "../SocialMedia";
+import TypeWriter from "./TypeWriter";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={6} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Kin</strong>
+              </h1>
+
+              <p className="heading-description blockquote">
+                
+Hi, I'm an experienced coding bootcamp graduate who has built serveral real-world projects. I'm proficient in using various technologies such as JavaScript, HTML, CSS, Node.js, Mysql, MongoDB, Express.js, and RESTful APIs.
+With my passion and dedication, I won't stop learning and improving.
+              </p>
+
+              <div className="heading-type">
+                <TypeWriter />
+              </div>
+            </Col>
+
+            <Col md={5}>
+              <img src={myImg} className="profile-pic" alt="avatar" />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container fluid className="home-about-section" id="about">
+        <Container>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>Get in Touch</h1>
+              <p>
+                {" "}
+                Whether you want to get in touch, or talk about a project
+                collaboration.
+                <br />
+                <strong>Feel free to connect with me</strong>
+              </p>
+              <SocialMedia />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Home;
